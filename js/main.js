1276458c313aff8faf49d144fc7ab1f68665ac8c
Nemes Mihai <mihai.nemes@3pillarglobal.com>
2014-10-30 17:52:58 +0200

    Add last mods

--- js/main.js
+++ js/main.js
@@ -1,16 +1,12 @@
 // the magic goes here :)
-function getJsonData(offset)
-{
-    var api = "http://aznitro-silver-test4.awsdev.telegraph.co.uk/content/sections/portal/subsections?limit=20&offset="+offset;
+function getJsonData(offset) {
+    var api = "http://aznitro-silver-test4.awsdev.telegraph.co.uk/content/sections/portal/subsections?limit=20&offset=" + offset;
     $.ajax({
         url: api,
         beforeSend: setHeader
     }).done(function( data ) {
-        console.log(data);
-        window.test = data;
         var data = data['@graph'][0];
         var lastOffset = parseInt(data.last['@id'].split('offset=')[1], 10);
-//        lastOffset = parseInt(offset,10);
         if($('.pagination').is(':empty')){
             var pages = Math.ceil(lastOffset/20);
             getPagination(pages,lastOffset);
@@ -18,7 +14,10 @@ function getJsonData(offset)
         $(".js_json_data").empty().attr('start',offset);
         var links = data.contains['@list'];
         $.each(links,function( index, value ){
-            $(".js_json_data").append("<li title='"+index+"'><a onclick='getSubSections('"+value.subsections['@id']+"')' href='avascript:void(0);'>"+value.label+"</a></li>");
+            var url = value.subsections['@id'].toString();
+            // console.log(getSubSections(url));
+            $(".js_json_data").append("<li  onclick='getSubSections(\""+url+"\")' class='list-group-item' title='"+index+"'><span class='badge'>"+index+"</span><span>"+value.label+"</span></li>");
+           //$(".js_json_data").append("<li title='"+index+"'><span data-url='"+url+"'>"+value.label+"</span></li>");
         });
         $('.js_data_btn').hide();
     });
@@ -37,13 +36,25 @@ function getPagination(pages,lastOffset) {
 
 function getSubSections(url){
     $.ajax({
-        url: url,
+        url: url.toString(),
         beforeSend: setHeader
-    }).done(function( data ) {
-        console.log(data);
+    }).done(function(data) {
+        var data = data['@graph'][0],
+            subs = data.contains['@list'];
+        $(".sub-section-list").show();
+        $(".js_json_subs").empty();
+        if(subs.length > 0){
+            $.each(subs, function(index,value){
+                $('.js_json_subs').append("<li class='list-group-item' title='"+index+"'><span class='badge'>"+index+"</span><span onclick='getSubSections(\""+url+"\")'>"+value.label+"</span></li>");
+            });
+        }else{
+             $('.js_json_subs').append("<li class='list-group-item'>No SubSections</li>");
+        }
     });
 }
 
 function setHeader(xhr) {
     xhr.setRequestHeader('accept', 'application/vnd.tcuk.content+core+ld+json');
 }
+
+// getSubSections('http://aznitro-silver-test4.awsdev.telegraph.co.uk/content/sections/0c127206-4dcc-4b0b-b30e-fb8bf4c95c82/subsections ');
